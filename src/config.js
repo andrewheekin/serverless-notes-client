1007f57c33df233b3daf099ac2ea2286ff3bfee1
Andrew Heekin <andrewheekin@gmail.com>
2017-06-07 19:33:59 -0400

    getting token back from cognito on login

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  cognito: {
+    USER_POOL_ID : 'us-east-1_2He7VEzOX',
+    APP_CLIENT_ID : '6t6ej2cgclvl6rm48in01m6tdo',
+  }
+};
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -5,7 +5,14 @@ import {
   FormControl,
   ControlLabel,
 } from 'react-bootstrap';
+import {
+  CognitoUserPool,
+  AuthenticationDetails,
+  CognitoUser
+} from 'amazon-cognito-identity-js';
 import './Login.css';
+import config from '../config.js';
+
 
 class Login extends Component {
   constructor(props) {
@@ -16,6 +23,27 @@ class Login extends Component {
       password: '',
     };
   }
+
+  login(username, password) {
+    const userPool = new CognitoUserPool({
+      UserPoolId: config.cognito.USER_POOL_ID,
+      ClientId: config.cognito.APP_CLIENT_ID
+    });
+    const authenticationData = {
+      Username: username,
+      Password: password
+    };
+
+    const user = new CognitoUser({ Username: username, Pool: userPool });
+    const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+    return new Promise((resolve, reject) => (
+      user.authenticateUser(authenticationDetails, {
+        onSuccess: (result) => resolve(result.getIdToken().getJwtToken()),
+        onFailure: (err) => reject(err),
+      })
+    ));
+  }  
 
   validateForm() {
     return this.state.username.length > 0
@@ -28,8 +56,16 @@ class Login extends Component {
     });
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    try {
+      const userToken = await this.login(this.state.username, this.state.password);
+      alert(userToken);
+    }
+    catch(e) {
+      alert(e);
+    }
   }
 
   render() {
