02cb36f2fb6453055c2b6eb30518e77299a5e7ff
Andrew Heekin <andrewheekin@gmail.com>
2017-06-08 15:02:25 -0400

    create note

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  apiGateway: {
+    URL: 'https://rspjd7ff2c.execute-api.us-east-1.amazonaws.com/prod',
+  },
   cognito: {
     USER_POOL_ID : 'us-east-1_2He7VEzOX',
     APP_CLIENT_ID : '6t6ej2cgclvl6rm48in01m6tdo',
